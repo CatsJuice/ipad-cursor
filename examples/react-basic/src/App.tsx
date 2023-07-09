1d434887f3a848b5c585e383e86af7eb22fcd1a7
CatsJuice <wp1298554944@hotmail.com>
2023-07-09 23:48:54 +0800

    feat(examples): add examples folder, add react example

---
+++ examples/react-basic/src/App.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { IPadCursorProvider, useIPadCursor } from "../../../src/react/index";
+import { IpadCursorConfig } from "../../../src";
+
+function App() {
+  const config: IpadCursorConfig = {
+    blockPadding: "auto",
+    blockStyle: {
+      radius: "auto",
+    },
+    enableAutoTextCursor: true,
+  };
+  useIPadCursor();
+  const [count, setCount] = useState(0);
+
+  return (
+    <IPadCursorProvider config={config}>
+      <a
+        className="inline-block rounded-30px"
+        data-cursor="block"
+        href="https://vitejs.dev"
+        target="_blank"
+      >
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a
+        className="inline-block rounded-30px"
+        data-cursor="block"
+        href="https://react.dev"
+        target="_blank"
+      >
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button data-cursor="block" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </IPadCursorProvider>
+  );
+}
+
+export default App;
---
+++ examples/react-basic/uno.config.ts
@@ -0,0 +1,30 @@
+// uno.config.ts
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  transformerDirectives,
+  transformerVariantGroup,
+} from "unocss";
+
+export default defineConfig({
+  // ...UnoCSS options
+  shortcuts: [
+    ['btn', 'px4 py1 rounded-md whitespace-nowrap bg-gray/20 hover:bg-gray/30 font-500'],
+    ['icon-btn', 'p3 rounded-md'],
+    ['full', 'w-full h-full'],
+    ['flex-center', 'flex items-center justify-center'],
+  ],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.2,
+      autoInstall: true,
+    }),
+    presetTypography(),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+});
